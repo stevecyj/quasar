6d6d2a6ed0125ff961e4ac04078a0d95cadc06d0
Steve <steve.tsao@hlw.lan>
2022-11-09 15:16:33 +0800

    feat:flex layout

---
+++ src/pages/FlexBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page padding>
+    <div class="box row q-gutter-md">
+      <div class="one">ssss</div>
+      <div class="two">ssss</div>
+      <div class="three">ss</div>
+    </div>
+  </q-page>
+</template>
+
+<style>
+.box {
+  width: 800px;
+  height: 800px;
+  border: 1px solid red;
+}
+
+.one {
+  width: 100px;
+  height: 100px;
+  border: 1px solid green;
+}
+
+.two {
+  width: 300px;
+  height: 300px;
+  border: 1px solid orange;
+}
+
+.three {
+  width: 200px;
+  height: 200px;
+  border: 1px solid blue;
+}
+</style>
