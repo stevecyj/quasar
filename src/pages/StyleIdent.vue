799d5d05083e8462cf21fb3c7f1a6a93d7d0e50a
Steve <steve.tsao@hlw.lan>
2022-11-09 14:09:42 +0800

    feat:toggle dark mode

---
+++ src/pages/StyleIdent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="text-h3 text-deep-purple-6 q-ma-sm-sm q-ma-md-xl ">
+    Lorem ipsum.
+  </div>
+  <div class="text-h3 text-deep-purple-6">
+    Lorem ipsum.
+  </div>
+  <button @click="toggleDark">change</button>
+</template>
+
+<script setup>
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+
+const toggleDark = () => {
+  $q.dark.toggle()
+}
+
+</script>
+
+<style>
+.box {
+  width: 600px;
+}
+</style>
