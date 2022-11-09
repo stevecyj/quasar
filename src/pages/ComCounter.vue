88ec5bca53f84924b572ef1196cbfe52e239b0a0
Steve <steve.tsao@hlw.lan>
2022-11-09 11:29:30 +0800

    feat:setup

---
+++ src/pages/ComCounter.vue
@@ -0,0 +1,37 @@
+<template>
+<h1>{{counter}}</h1>
+<h1 class="text_color">{{doubleCount}}</h1>
+<button @click="add">add</button>
+</template>
+
+<script>
+import { computed, defineComponent, ref, watch } from 'vue'
+
+export default defineComponent({
+  setup () {
+    const counter = ref(0)
+    const color = ref('red')
+
+    const add = () => { counter.value++ }
+
+    watch(counter, (newVal, oldVal) => {
+      if (newVal % 2 === 0) { color.value = 'red' } else { color.value = 'green' }
+    })
+
+    const doubleCount = computed(() => { return counter.value * 2 })
+
+    return {
+      counter,
+      doubleCount,
+      color,
+      add
+    }
+  }
+})
+</script>
+
+<style>
+.text_color{
+  color: v-bind(color)
+}
+</style>
---
+++ src/pages/SetupCounter.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>{{counter}}</h1>
+<h1 class="text_color">{{doubleCount}}</h1>
+<button @click="add">add</button>
+</template>
+
+<script setup>
+import { computed, ref, watch } from 'vue'
+
+const counter = ref(0)
+const color = ref('red')
+
+const add = () => { counter.value++ }
+
+watch(counter, (newVal, oldVal) => {
+  if (newVal % 2 === 0) { color.value = 'red' } else { color.value = 'green' }
+})
+
+const doubleCount = computed(() => { return counter.value * 2 })
+</script>
+
+<style>
+.text_color{
+  color: v-bind(color)
+}
+</style>
