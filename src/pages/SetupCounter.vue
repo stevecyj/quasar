f85df3bcf91ad76b39d6ffacf1dafb12f8f017f0
Steve <ycsteven.tsao@gmail.com>
2022-11-11 07:44:51 +0800

    feat:use router

--- src/pages/SetupCounter.vue
+++ src/pages/SetupCounter.vue
@@ -2,11 +2,14 @@
   <h1>{{ counter }}</h1>
   <h1 class="text_color">{{ doubleCount }}</h1>
   <button @click="add">add</button>
+  <button @click="toStyle" label="to">toIndex</button>
 </template>
 
 <script setup>
 import { computed, ref, watch } from 'vue'
+import { useRouter } from 'vue-router'
 
+const $router = useRouter()
 const counter = ref(0)
 const color = ref('red')
 
@@ -21,6 +24,10 @@ watch(counter, (newVal, oldVal) => {
     color.value = 'green'
   }
 })
+
+const toStyle = () => {
+  $router.push('index')
+}
 
 const doubleCount = computed(() => {
   return counter.value * 2
